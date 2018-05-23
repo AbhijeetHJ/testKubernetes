e11157423a06e734a950d07542d76cde65035aeb
abhijeet_jadhav <abhijeet_jadhav>
2018-05-23 19:03:36 +0530

    initial commit

---
+++ src/main/java/com/kubernetes/kubernetes/KubernetesApplication.java
@@ -0,0 +1,20 @@
+package com.kubernetes.kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class KubernetesApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesApplication.class, args);
+	}
+}
